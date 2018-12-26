4b5bc27b4503cb73b97aa12ff4cc624bcf695132
dogan <dgnkrhan@gmail.com>
2018-12-26 09:43:17 +0300

    post add

--- login.php
+++ login.php
@@ -6,22 +6,6 @@ $password = mysqli_real_escape_string($db,$_POST['password']);
 
 if ($username != "" && $password != ""){
 
-    /*$sql_query = "SELECT id FROM Users WHERE username=\"$username\" AND password=\"$password\"";
-    $result = mysqli_query($con,$sql_query);
-    $row = mysqli_fetch_array($result);
-
-    $count = $row['cntUser'];
-
-    if($count > 0){
-        $_SESSION['username'] = $username;
-        echo 1;
-        echo ("successfuly login");
-    }else{
-        echo 0;
-        echo ("unsuccessfuly login");
-    }
-*/
-
     $sql_query = "SELECT id FROM Users WHERE ( username=\"$username\" OR email=\"$email\") AND password=\"$password\"";
     $result = mysqli_query($db, $sql_query);
     $row = mysqli_fetch_array($result);
@@ -30,12 +14,16 @@ if ($username != "" && $password != ""){
 
     if ($count > 0) {
         $row = mysqli_fetch_assoc($result);
-        echo 1;
+        //echo 1;
+        echo ($count);
         //echo ('{"userid":' . $row["id"] . ', "islogin":true, "err":""}');
     } else {
         echo 0;
         //echo ('{"userid":-1, islogin":false, "err":"User not found."}');
+        //echo ('{"userid":-1, islogin":false, "err":"User not found."}');
     }
+
+    mysqli_close($db);
 
 }
 ?>
